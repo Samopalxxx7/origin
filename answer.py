362bfb357369f138134ab5ac20ac153dd6333bc6
Samopalxxx7 <burun_ilya@ukr.net>
2020-06-12 12:36:48 +0300

    Fuck git hub

---
+++ answer.py
@@ -0,0 +1,23 @@
+print"answer"
+
+
+number = 42
+
+
+while True:
+	answer = input("11111?")
+	if not answer or answer == "exit":
+		break
+	if not answer.isdigit():
+		print("Another one?")
+		continue
+
+	user_answer = int(answer)
+	if user_answer > number:
+		print("Number is more")
+	elif user_answer < number:
+		print("Number is less")
+	else:
+		print("Congratulations")
+		break
+input()
