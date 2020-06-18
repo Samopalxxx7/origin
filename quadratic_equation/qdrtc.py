f09d86d6de5d09e3314ab7611087415c8853cf01
Samopalxxx7 <burun_ilya@ukr.net>
2020-06-18 11:36:58 +0300

    4th commit changed sqrt

--- quadratic_equation/qdrtc.py
+++ quadratic_equation/qdrtc.py
@@ -7,8 +7,18 @@ b = int(sys.argv[2])
 c = int(sys.argv[3])
 print(a,b,c)
 
-k2 = b**2 - 4*a*c
-print(k2)
-x1 = ((-b + math.sqrt(k2))/(2*a))
-x2 = ((-b - math.sqrt(k2))/(2*a))
-print(int(x1), int(x2))
+discrim = b**2 - 4*a*c
+
+
+if discrim <0:
+	print("without sqrt")
+elif discrim == 0:
+	x1 = -b / (2*a)
+	x2 = x1
+	print(x1, x2)
+else:
+	x1 = (-b + math.sqrt(discrim)) / (2 * a)
+	x2 = (-b - math.sqrt(discrim)) / (2 * a)
+	print("!!!!!!!!!!!!!!!!!!!!!")
+	print(math.trunc(x1))
+	print(math.trunc(x2))
